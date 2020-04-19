34d757fa5c2b72ef10ab8404efaf3c4f57c7741c
Martin Chaine <martin@casimir.me>
2020-04-20 00:05:37 +0200

    implement long polling correctly

--- api/clientserverr060/impl.go
+++ api/clientserverr060/impl.go
@@ -16,8 +16,26 @@ func getPushRules(ctx context.Context) (GetPushRulesResponse, error) {
 }
 
 func sync(ctx context.Context, query url.Values) (SyncResponse, error) {
-	resp := SyncResponse{
-		NextBatch: "next123",
+	timeout := query.Get("timeout")
+	if timeout == "" {
+		timeout = "0"
+	}
+	timeoutMs, err := strconv.Atoi(timeout)
+	if err != nil {
+		return SyncResponse{}, common.New("invalid timeout")
+	}
+
+	done := make(chan bool, 1)
+	resp := SyncResponse{}
+	go func() {
+		time.Sleep(1000 * time.Second)
+		done <- true
+	}()
+	select {
+	case <-done:
+		log.Print("got a new event")
+	case <-time.After(time.Duration(timeoutMs) * time.Millisecond):
+		resp.NextBatch = strconv.FormatInt(time.Now().Unix(), 10)
 	}
 	return resp, nil
 }
